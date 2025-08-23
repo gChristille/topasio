ea52e94d523a8ecd0b34a5197b0a92f5ecf824e6
PinoLG01 <zuello.01@gmail.com>
2025-08-23 18:03:22 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 DESCRIPTION = 'Package to automate writing of (Open)TOPAS scripts, allowing for type inference and better access to parameters'
 
 
